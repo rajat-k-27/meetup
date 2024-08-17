6ac9fe216460dd9356a199cdbe9ed2de3b2a6f10
rajat-k-27 <155521364+rajat-k-27@users.noreply.github.com>
2024-08-17 21:26:02 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -8,13 +8,7 @@ const Home = () => {
   const time= now.toLocaleTimeString('en-US', {
     hour:'2-digit', minute:'2-digit'
   });
-  const date = now.toLocaleDateString('en-US', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-  });
-
+ const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
