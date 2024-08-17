f669e6683dc547cccf7f111aed9746315453736d
rajat-k-27 <155521364+rajat-k-27@users.noreply.github.com>
2024-08-17 21:29:13 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 
 const Home = () => {
 
-  const now = new Date();
+const now = new Date();
 
-  const time= now.toLocaleTimeString('en-US', {
-    hour:'2-digit', minute:'2-digit'
-  });
- const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', timeZone: 'Asia/Kolkata' });
+const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeZone: 'Asia/Kolkata' })).format(now);
 
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
